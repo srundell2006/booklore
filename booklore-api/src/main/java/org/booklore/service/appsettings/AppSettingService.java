8379482159b0fe89f8d43584b871b8c158bd7c59
srundell2006 <stephen.rundell@gmail.com>
2026-07-27 16:28:52 -0500

    Wire bookdrop auto-import settings into buildAppSettings()

--- booklore-api/src/main/java/org/booklore/service/appsettings/AppSettingService.java
+++ booklore-api/src/main/java/org/booklore/service/appsettings/AppSettingService.java
@@ -190,6 +190,23 @@ public class AppSettingService {
         builder.maxFileUploadSizeInMb(Integer.parseInt(settingPersistenceHelper.getOrCreateSetting(AppSettingKey.MAX_FILE_UPLOAD_SIZE_IN_MB, "100")));
         builder.metadataDownloadOnBookdrop(Boolean.parseBoolean(settingPersistenceHelper.getOrCreateSetting(AppSettingKey.METADATA_DOWNLOAD_ON_BOOKDROP, "true")));
 
+        builder.bookdropAutoImportEnabled(Boolean.parseBoolean(settingPersistenceHelper.getOrCreateSetting(AppSettingKey.BOOKDROP_AUTO_IMPORT_ENABLED, "false")));
+        builder.bookdropAutoImportMinScore(Integer.parseInt(settingPersistenceHelper.getOrCreateSetting(AppSettingKey.BOOKDROP_AUTO_IMPORT_MIN_SCORE, "50")));
+        String autoImportLibraryIdStr = settingsMap.get(AppSettingKey.BOOKDROP_AUTO_IMPORT_LIBRARY_ID.getDbKey());
+        if (autoImportLibraryIdStr != null && !autoImportLibraryIdStr.isBlank()) {
+            try {
+                builder.bookdropAutoImportLibraryId(Long.parseLong(autoImportLibraryIdStr));
+            } catch (NumberFormatException ignored) {
+            }
+        }
+        String autoImportPathIdStr = settingsMap.get(AppSettingKey.BOOKDROP_AUTO_IMPORT_PATH_ID.getDbKey());
+        if (autoImportPathIdStr != null && !autoImportPathIdStr.isBlank()) {
+            try {
+                builder.bookdropAutoImportPathId(Long.parseLong(autoImportPathIdStr));
+            } catch (NumberFormatException ignored) {
+            }
+        }
+
         String sessionDurationStr = settingsMap.get(AppSettingKey.OIDC_SESSION_DURATION_HOURS.getDbKey());
         if (sessionDurationStr != null && !sessionDurationStr.isBlank()) {
             try {
