5c63863f8b244758275032157d233dcae09e305a
srundell2006 <stephen.rundell@gmail.com>
2026-07-27 16:24:53 -0500

    Add V144 migration: bookdrop auto-import settings

---
+++ booklore-api/src/main/resources/db/migration/V144__Add_bookdrop_auto_import_settings.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO app_setting (name, val) VALUES ('bookdrop_auto_import_enabled', 'false');
+INSERT IGNORE INTO app_setting (name, val) VALUES ('bookdrop_auto_import_min_score', '50');
